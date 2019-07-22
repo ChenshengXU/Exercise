ebdabd4349115b42a48c4b0d4b0c8c4dfd78edc0
ChenshengXU <34076622+ChenshengXU@users.noreply.github.com>
2019-07-22 15:03:41 +0200

    Add files via upload

---
+++ Element.cpp
@@ -0,0 +1,42 @@
+using namespace std;
+
+#include "Element.h"
+#include <fstream>
+#include <iostream>
+
+#include <algorithm>
+
+Element::Element(string id_) {
+        id = id_;
+        T = 0;
+        T_start = 0;
+
+        cout << "Created Element: " << id << "; T=" << T << endl;
+}
+
+Element::Element(string id_, float T_start_) {
+        id = id_;
+        T = T_start_;
+        T_start = T_start_;
+
+        cout << "Created Element: " << id << "; T=" << T << endl;
+}
+
+
+void Element::setT(float T_new) {
+        T = T_new;
+}
+
+float Element::getT() {
+        return T;
+}
+
+void Element::fout(fstream& output_file, float time, float dTem) {
+        output_file << time << ";" << T << ";" << dTem << endl;
+}
+
+//void Element::fout(fstream& output_file, float time) {
+//        output_file << time << ";" << T << endl;
+//}
+
+
